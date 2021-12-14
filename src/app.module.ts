420995e6078095a437565e4fb4d896eb7cc140e6
zmj <hellozmj@qq.com>
2021-12-15 00:50:27 +0800

    feat: finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { App2Service } from './app2.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, App2Service],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { App2Service } from './app2.service';
 
-@Injectable()
+// @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(app2: App2Service) {
+    console.log(typeof app2);
   }
 }
